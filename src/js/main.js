1845ec04e7e729e7e5cd962d5ddd96864dbfd57c
Kyrubi <u20221c486@upc.edu.pe>
2025-10-06 22:30:56 -0500

    feat: add FAQ section with accordion functionality and styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 import { initializeLanguageSelector } from "./modules/i18n/language-controller.js";
 import { initializeMenuToggle } from "./modules/ui/menu-toggle.js";
 import { initializeTilt } from "./modules/ui/tilt.js";
+import {initAccordion} from "./modules/ui/accordion.js";
 import '../css/main.css'
 
 document.addEventListener('DOMContentLoaded', async () => {
     await initializeLanguageSelector();
     initializeMenuToggle();
     initializeTilt();
+    initAccordion()
 })
+
